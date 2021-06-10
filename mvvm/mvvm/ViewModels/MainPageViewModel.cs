64b060d9a1040a921c2a3b719a0745935acab46f
tyasin <2389092255@qq.com>
2021-06-11 00:41:51 +0800

    temp

---
+++ mvvm/mvvm/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mvvm.ViewModels
+{
+    public class MainPageViewModel
+    {
+    }
+}
---
+++ mvvm/mvvm/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,14 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+
+namespace mvvm.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public MainPageViewModel MainPageViewModel => SimpleIoc.Default.GetInstance<MainPageViewModel>();
+        public ViewModelLocator()
+        {
+            SimpleIoc.Default.Register<MainPageViewModel>();
+        }
+    }
+}
+
