1b2b9148363dfd97654f335314714bf70c19eebe
git-yasins <2389092255@qq.com>
2021-06-11 16:08:52 +0800

    binding

--- mvvm/mvvm/ViewModels/MainPageViewModel.cs
+++ mvvm/mvvm/ViewModels/MainPageViewModel.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace mvvm.ViewModels
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : ViewModelBase
     {
+        private string _result;
+        public string Result {
+            get => _result;
+            set => Set(nameof(Result), ref _result, value);
+        }
+
+        private RelayCommand _command;
+        public RelayCommand Command => _command ?? new RelayCommand(() => Result = "test");
     }
 }
